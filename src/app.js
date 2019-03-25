7cb27918908f075a306e981c9955d40dfca3a1b7
May <may.johansson@hva.nl>
2019-03-25 16:59:26 +0100

    add styling

--- src/app.js
+++ src/app.js
@@ -1,23 +1,9 @@
 'use strict'
 
 const ejs = require('ejs')
-const mongo = require('mongodb')
-const mongoose = require('mongoose')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-
-require('dotenv').config()
-
-const connectDb = function () {
-  return mongoose.connect(process.env.DB_URL, {useNewUrlParser: true})
-}
-
-connectDb().then(async () => {
-  app.listen(process.env.PORT, () =>
-    console.log(`Listening on port ${process.env.DB_PORT}!`)
-  )
-})
 
 app
   .use(express.static('static'))
@@ -30,38 +16,6 @@ app
   .get('/:name', detail)
   .post('/:name', addProduct)
   .listen(1999)
-
-const createListWithProduct = async function () {
-  const list1 = new models.List({
-    listName: 'first',
-  })
-
-  const list2 = new models.List({
-    listName: 'second',
-  })
-
-  const product1 = new models.Product({
-    productName: 'Tomaten',
-    listName: list1.id,
-  })
-
-  const product2 = new models.Product({
-    productName: 'Bier',
-    listName: list2.id,
-  })
-
-  const product3 = new models.Product({
-    productName: 'Brood',
-    listName: list2.id,
-  })
-
-  await product1.save()
-  await product2.save()
-  await product3.save()
-
-  await list1.save()
-  await list2.save()
-}
 
 const list = {
   list: ["Ontbijt", "Lunch", "Avondeten"]
